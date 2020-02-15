202d5cc7891545721dfb56ce5bc78fd03cb2107f
bomen <bomenuk1@gmail.com>
2020-02-15 23:21:34 +0000

    First failed unit test for CourseRepository

--- RepositoryTest.Tests/CourseRepositoryTests.cs
+++ RepositoryTest.Tests/CourseRepositoryTests.cs
@@ -4,6 +4,21 @@ namespace RepositoryTest.Tests
 {
     public class CourseRepositoryTests
     {
+        CourseRepository courseRepository = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            courseRepository = new CourseRepository();
+        }
+
+        [Test]
+        public void Get_Should_Return_Null_When_No_Records()
+        {
+            var result = courseRepository.Get("math");
+            Assert.IsNull(result);
+        }
+
         
     }
 }
